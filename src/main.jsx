9985aabe989a30d7f9182d966fd0252e68ae68ae
Jonatan Morales <developerjonatanmorales@gmail.com>
2023-03-05 12:22:36 -0600

    importando css personalizados

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import './main.css'
 import App from './App'
+
 import { Header } from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
