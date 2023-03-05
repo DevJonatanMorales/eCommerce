54dcdeceaf8abd698a8859254b69af7f9ffc05eb
Jonatan Morales <developerjonatanmorales@gmail.com>
2023-03-05 14:44:31 -0600

    Encabezado

--- src/components/ShowCategorias.jsx
+++ src/components/ShowCategorias.jsx
@@ -9,17 +9,16 @@ export const ShowCategorias = () => {
     allCategorias(setCategorias);
   }, []);
 
-console.log(categorias);
-
   return (
-    <div className="container text-center">
+    <div className="container  text-center">
+      <h1 className="Categorias">Nuestras Categorias</h1>
       <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
         {
           categorias != null ? 
             categorias.map((categoria) => (
-              <div key={categoria.id} class="bg-card col card my-2 mx-auto text-center bg-body-secondary" style={{width: '20rem'}}>
-                <img src={categoria.imagenes.normal} class="card-img-top m-3" style={{width: '18rem'}} alt="Error al cargar"/>
-                <div class="card-body">
+              <div key={categoria.id} class="bg-card col card my-2 p-3 mx-auto text-center bg-body-secondary" style={{width: '21rem'}}>
+                <img src={categoria.imagenes.normal} class="card-img-top mx-auto" style={{width: '18rem'}} alt="Error al cargar"/>
+                <div class="card-body m-0">
                   <a className="btn btn-dark" href={`/catagoria/${categoria.id}`} > {categoria.nombre} </a>
                 </div>
               </div>
