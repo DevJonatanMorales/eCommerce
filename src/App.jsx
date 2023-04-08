6b0ad7f8f2dea52a0fa32bff0ba5f494e658f4d8
Jonatan Morales <developerjonatanmorales@gmail.com>
2023-04-07 20:44:12 -0600

    ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ShowCategorias } from './components/ShowCategorias'
-import { ShowProducts } from './components/ShowProducts'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ShowCategorias } from "./components/ShowCategorias";
+import { ShowProducts } from "./components/ShowProducts";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<ShowCategorias></ShowCategorias>} ></Route>
-        <Route path='/catagoria/:categoria/nombre/:nombre' element={ <ShowProducts></ShowProducts>} ></Route>
+        <Route
+          path="/eCommerce/"
+          element={<ShowCategorias></ShowCategorias>}
+        ></Route>
+        <Route
+          path="/eCommerce/catagoria/:categoria/nombre/:nombre"
+          element={<ShowProducts></ShowProducts>}
+        ></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
