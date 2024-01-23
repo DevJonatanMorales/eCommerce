cc528615e8b84873a082c579279110bf899ce87e
Jonatan Morales <developerjonatanmorales@gmail.com>
2024-01-23 03:10:30 -0600

    refactor: funciones de ayuda

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,5 @@
-import axios from "axios";
-import { useState } from "react";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
-
-const url = 'http://173.254.242.213:8080/clientapp-web/webresources/getMenus/APP'
 
 const allCategorias = (state) => {
     fetch(url)
@@ -22,20 +18,11 @@ export const ShowAlert = (msj, icon, focu='') => {
     const MySwal = withReactContent(Swal);
 
     MySwal.fire({
-        title: 'eCommer',
+        title: 'ADMINISTRACION',
         text: msj,
         confirmButtonText: "Aceptar",
         icon: icon
     })
-}
-
-export const ProductsCounter = () => {
-    const [count, setCount] = useState(1);
-    const increment = () => setCount( count + 1);
-    const decrement = () => setCount( count - 1);
-    const reset = () => setCount(1)
-
-    return { count, increment, decrement, reset}
 }
 
 export {
